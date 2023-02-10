7193d084845b9ef4d3705646df2462bf0537aae7
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-02-10 13:26:35 +0200

    create

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {Login} from './component/login.jsx'
+function App() {
+  return (
+    <div className='container pt-5'>
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/login.jsx
@@ -0,0 +1,10 @@
+export const Login =()=>{
+    return(
+<form action="form " className="d-flex">
+    <input className="form-control" type="text" placeholder="Login"/>
+    <input className="form-control" type="password" placeholder="Password"/>
+    <button className="btn btn-primary" type="submit"> Submit</button>
+</form>
+    )
+}
+
